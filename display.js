0368964b39ff531db97dd001e61efc6a4676c220
mitalijawarkar <63419159+mitalijawarkar@users.noreply.github.com>
2020-04-20 16:14:15 +0530

    For Question 1

---
+++ display.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import nameprint from './nameprint';
+
+class display extends React.Component{    
+    render(){
+        var Name ='yash';
+        return(
+            <div>
+                <nameprint/>
+            </div>
+        )
+    }
+}
+
+export default display
---
+++ nameprint.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class nameprint extends React.Component{    
+    render(){
+        var Name ='yash';
+        return(            
+                <h1>Hello {Name} </h1>            
+        )
+    }
+}
+
+export default nameprint
